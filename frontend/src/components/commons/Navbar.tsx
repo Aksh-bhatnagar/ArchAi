bb9aa7b2a2c7dd2bd634a6b9ed99ede2601a8ee8
Aksh Bhatnagar <akshbhatnagar2111@gmail.com>
2026-01-15 08:35:42 +0530

    "refine navbar"

--- frontend/src/components/commons/Navbar.tsx
+++ frontend/src/components/commons/Navbar.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 type Props = {
   mode: "login" | "signup";
@@ -9,8 +9,9 @@ type Props = {
 export default function Navbar({ mode, setMode }: Props) {
   const location = useLocation();
   const [isDisabled, setIsDisabled] = useState(false);
+  const navigate = useNavigate();
 
-  const handleClick = () => {
+  const handleTransform = () => {
     if(isDisabled) return;
 
     setIsDisabled(true)
@@ -22,18 +23,20 @@ export default function Navbar({ mode, setMode }: Props) {
     setMode(mode === "login" ? "signup" : "login")
   }
 
+  const handleLogo = () => navigate("/")
+
   return (
     <div className="bg-[#0F172A] text-[#E5E7EB] h-18 w-screen top-0 flex absolute z-20 justify-between">
       <div className="flex">
-        <h3 className="text-3xl py-4 pl-4 font-bold text-white">Arch</h3>
-        <h3 className="text-3xl py-4 pr-4 font-bold text-sky-400">AI</h3>
+        <h3 className="text-3xl py-4 pl-4 font-bold text-white cursor-pointer" onClick={handleLogo}>Arch</h3>
+        <h3 className="text-3xl py-4 pr-4 font-bold text-sky-400 cursor-pointer" onClick={handleLogo}>AI</h3>
       </div>
 
       <div className="flex justify-center items-center">
         {location.pathname === "/auth" && (
         <span
           className={`bg-gray-700 px-5 py-2 rounded-md text-white w-full m-3 ${ isDisabled ? "opacity-50 cursor-not-allowed" : "cursor-pointer" } hover:bg-gray-600`}
-          onClick= {handleClick}
+          onClick= {handleTransform}
         >
           {mode === "login" ? "Signup" : "Login"}
         </span>
