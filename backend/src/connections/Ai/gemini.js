2ec258e155f4ede510b7dd4325fb06863b3a8f9b
Aksh Bhatnagar <akshbhatnagar2111@gmail.com>
2026-07-21 21:10:26 +0530

    test1

--- backend/src/connections/Ai/gemini.js
+++ backend/src/connections/Ai/gemini.js
@@ -1,5 +1,7 @@
 import {GoogleGenAI} from '@google/genai';
+import dotenv from "dotenv";
 
+dotenv.config();
 const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
 
 const AI = new GoogleGenAI({apiKey: GEMINI_API_KEY});
